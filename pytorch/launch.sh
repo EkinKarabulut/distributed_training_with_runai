409178fe291276e0ab870395747f7c50d5d70c76
EkinKarabulut <82878945+EkinKarabulut@users.noreply.github.com>
2025-05-19 14:09:41 +0200

    Update launch.sh

--- pytorch/launch.sh
+++ pytorch/launch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-master_addr=$MASTER_ADDR
-master_port=$MASTER_PORT
-job_n=$WORLD_SIZE
-#job_id=$RANK
+echo "Using configuration:"
+echo "  MASTER_ADDR:   $MASTER_ADDR"
+echo "  MASTER_PORT:   $MASTER_PORT"
+echo "  WORLD_SIZE:    $WORLD_SIZE"
+echo "  PET_NPROC_PER_NODE: $PET_NPROC_PER_NODE"
 
-# Echo these if needed
-#echo ${job_n}
-#echo ${job_id}
-#echo ${master_addr}
-#echo ${master_port}
-
-torchrun --nproc_per_node=1 --nnodes=${job_n} --rdzv_endpoint=${master_addr}:${master_port} --rdzv_backend=c10d distributed.py --batch_size 8 30 2
+torchrun --nproc_per_node=1 \
+  --nnodes="$WORLD_SIZE" \
+  --rdzv_endpoint="${MASTER_ADDR}:${MASTER_PORT}" \
+  --rdzv_backend=c10d \
+  distributed.py \
+  --batch_size 8 30 2
